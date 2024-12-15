ae89ba75c62d6e886eb9bb2a60eaad3fa4633650
Ernesto Cruz Olivera <ecruzolivera@gmail.com>
2024-12-15 16:51:11 -0600

    adding deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://ecruzolivera.github.io',
+  base: 'Notas-de-Rol',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
